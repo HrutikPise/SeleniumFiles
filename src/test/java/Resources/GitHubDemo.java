9dc88b1a98467e48c4b63da409040dd22aef361f
Pise, Hrutik <hrutikpise8@gmail.com>
2023-01-03 18:20:12 +0530

    Added GitHubDemo.java

---
+++ src/test/java/Resources/GitHubDemo.java
@@ -0,0 +1,10 @@
+package Resources;
+
+public class GitHubDemo {
+	public static void main(String[] args) {
+		System.out.println("Hello world");
+		System.out.println("Hello world");
+		System.out.println("Hello world");
+		System.out.println("Hello world");
+	}
+}
